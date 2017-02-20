e9256fe13c9e102515117f2fb1d82acee050cd4a
NikeshSuwal <nikesh@jyaasa.com>
2017-02-20 13:14:34 +0545

    give width of rate icon and change div into span of rating group in rating_helper
    Summary: change the star icons and reduced font size of review content
    
    Reviewers: #builo, nelson
    
    Reviewed By: #builo, nelson
    
    Maniphest Tasks: T2039
    
    Differential Revision: http://phabricator.jyaasa.com/D2261

--- app/helpers/rating_helper.rb
+++ app/helpers/rating_helper.rb
@@ -2,11 +2,11 @@
 module RatingHelper
   def show_rating(rate_star)
     rate_star = rate_star.round(0) if rate_star.present?
-    content_tag :div, '', class: 'star', 'data-rating' => rate_star,
+    content_tag :span, '', class: 'star', 'data-rating' => rate_star,
                                          'data-readonly' => true,
                                          'data-star-count'=> 5,
                                          'data-star-path' => '/assets',
-                                         'data-star-on' => 'star-off.png',
-                                         'data-star-off' => 'star.png'
+                                         'data-star-on' => 'star-on.png',
+                                         'data-star-off' => 'star-off.png'
   end
 end
